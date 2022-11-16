5a439359f6a676f56ef48aec2251ba3c60e68323
UandersonFerreira <103908858+UandersonFerreira@users.noreply.github.com>
2022-11-16 14:42:20 -0300

    commit

--- src/main/java/com/uanderson/Calculadora.java
+++ src/main/java/com/uanderson/Calculadora.java
@@ -1,46 +1,51 @@
 package com.uanderson;
 
-import java.util.IllegalFormatCodePointException;
 
 public class Calculadora {
     int max_int = Integer.MAX_VALUE;
     int min_int = Integer.MIN_VALUE;
     //int n = 2147483649; parametiza test
 
-    public int somar(int valor1, int valor2){
-        if (valor1 >= max_int || valor1 <= min_int || valor2 >= max_int || valor2 <= min_int){
-            throw new IllegalArgumentException("Valor das parcelas maior que o permitido!");
+    public int somar(int parcela1, int parcela2){
+        valorLimiteInteiros(parcela1, parcela2);
+        return parcela1 + parcela2;
+    }
+
+    private void valorLimiteInteiros(int parcela1, int parcela2) {
+        if (parcela1 >= max_int || parcela1 <= min_int || parcela2 >= max_int || parcela2 <= min_int){
+            throw new IllegalArgumentException("Valor das parcelas maior/menor que o permitido! (MAX = 2147483647 | MIX = -2147483648 )");
         }
-        return valor1 + valor2;
     }
 
-    public int subtrair(int valor1, int valor2){
-        return valor1 - valor2;
+    public int subtrair(int parcela1, int parcela2){
+        return parcela1 - parcela2;
     }
 
-    public int multiplicar(int valor1, int valor2){
-        return valor1 * valor2;
+    public int multiplicar(int parcela1, int parcela2){
+        valorLimiteInteiros(parcela1, parcela2);
+        return parcela1 * parcela2;
     }
 
-    public int dividir(int valor1, int valor2){
-        if (valor1 == 0 || valor2 == 0){
+    public int dividir(int dividendo, int divisor){
+        if (divisor == 0){//não é permitido somente quando o divisor é zero
             throw new ArithmeticException("Divisão por 0 não é Permitida");
         }
-        return valor1 / valor2;
+        return dividendo/divisor;
     }
 
     public static void main(String[] args) {
         Calculadora c = new Calculadora();
-       // System.out.println( c.dividir(0,0));
 
         System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
 
+        System.out.println( c.somar(2147483647,5));
 
     }
 }//class
 
 /*
- if (valor1 == 0 || valor2 == 0){
+ if (parcela1 == 0 || parcela2 == 0){
             throw new ArithmeticException("Divisão por 0 não é Permitida");
         }
  */
--- src/test/java/com/uanderson/CalculadoraTest.java
+++ src/test/java/com/uanderson/CalculadoraTest.java
@@ -26,16 +26,33 @@ public class CalculadoraTest {
     @Test
     @DisplayName("Divisão de dois Inteiros")
     public void dividirInteiros(){
-        Assertions.assertEquals(2, calculadora.dividir(6,3));
+        Assertions.assertEquals(0, calculadora.dividir(0,3));
     }
 
     @Test
-    @DisplayName("Divisão de dois Inteiros")
+    @DisplayName("Divisão de dois Inteiros Por zero")
     public void dividirInteiroPorZero(){
         ArithmeticException exception = Assertions.assertThrows(ArithmeticException.class, () -> {
             calculadora.dividir(5, 0);
         });
         Assertions.assertEquals("Divisão por 0 não é Permitida", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Soma de valores máximos e Minimos")
+    public void somarValoreMaximoseMininos(){
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculadora.somar(2147483, -2147483648);
+        });
+        Assertions.assertEquals("Valor das parcelas maior/menor que o permitido! (MAX = 2147483647 | MIX = -2147483648 )", exception.getMessage());
+    }
+    @Test
+    @DisplayName("Multiplicação de valores máximos e Minimos")
+    public void multiplicacaoValoreMaximoseMininos(){
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculadora.somar(2147483, -2147483648);
+        });
+        Assertions.assertEquals("Valor das parcelas maior/menor que o permitido! (MAX = 2147483647 | MIX = -2147483648 )", exception.getMessage());
     }
 
 
